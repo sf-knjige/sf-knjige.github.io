a35e7a96297f0378880cee1a1e2ef29d954d613a
Damjan Pavlica <mudroljub@gmail.com>
2019-12-22 12:57:38 +0100

    mobi

--- tools.js
+++ tools.js
@@ -1,24 +1,35 @@
-var path = require('path');
-var fs = require('fs');
+const path = require('path')
+const fs = require('fs')
 const cheerio = require('cheerio')
 
-const dir = './knjige/1984/'
-let knjiga = ''
+const dir = './knjige/'
+const subdir = '1984'
 
-const getNum = str => {
-  return str.match(/\d+/) ? str.match(/\d+/)[0] : 0
-}
+const getNum = str => str.match(/\d+/) ? str.match(/\d+/)[0] : 0
 
-fs.readdir(dir, function (err, files) {
-    if (err) return console.log('Error scan directory: ' + err);
-    files
-      .filter(x => x !== 'index0.html' && x !== 'naslov.html')
-      .sort((a, b) => getNum(a) - getNum(b))
-      .forEach(function (file) {
-        const sadrzaj = fs.readFileSync(dir + file, 'utf8')
-        const $ = cheerio.load(sadrzaj)
-        knjiga += $('body').text()
-    });
-    console.log(knjiga);
-    fs.writeFileSync('knjiga.txt', knjiga)
-});
+fs.readdir(dir, (err, allBooks) => {
+  if (err) return console.log('Error scan directory: ' + err)
+  allBooks
+    .filter(x => !x.includes('.html'))
+    .forEach(subdir => {
+      console.log(subdir)
+      let knjiga = ''
+      const pages = fs.readdirSync(dir + subdir)
+
+      const naslovHtml = fs.readFileSync(dir + subdir + '/naslov.html', 'utf8')
+      const $naslovHtml = cheerio.load(naslovHtml)
+      const naslov = $naslovHtml('title').text().replace(/\//g, ' & ')
+      console.log(naslov)
+
+      pages
+        .filter(x => x !== 'index0.html' && x !== 'naslov.html' && x !== 'menu.html')
+        .sort((a, b) => getNum(a) - getNum(b))
+        .forEach(file => {
+          const strana = fs.readFileSync(dir + subdir + '/' + file, 'utf8')
+          const $strana = cheerio.load(strana)
+          knjiga += $strana('body').text()
+        })
+      fs.writeFileSync(`mobi/${naslov}.txt`, knjiga)
+
+    })
+})
